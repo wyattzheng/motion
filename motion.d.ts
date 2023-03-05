40c6d5bd1c1f2bc40575dd9b3fa40f4671984faa
wyattzheng <854185073@qq.com>
2023-03-05 14:01:46 +0800

    feat: 增加代码提示和检查

---
+++ motion.d.ts
@@ -0,0 +1,20 @@
+interface CreateOption {
+    loop: boolean
+}
+
+type BuilderFunction = (name: string, style?: any, children?: string[]) => string;
+type ActionCallback = (percent: number) => any;
+
+interface MotionAnimation{
+    constructor(option?: CreateOption);
+    action(rangeOrFn: [number, number] | number | ActionCallback, fn?: ActionCallback): void;
+    play(): void;
+    goto(timestamp: number): void;
+    pause(): void;
+    stop(): void;
+}
+
+declare var animation: (option?: CreateOption) => MotionAnimation;
+declare var div: BuilderFunction;
+declare var move: (className: string, x: number, y: number) => void;
+declare var text: (className: string, content: string) => void;
